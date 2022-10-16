ffbbfbe2e509e69627b81393522c70ca68503c94
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-16 19:47:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,25 +14,15 @@ const DEBOUNCE_DELAY = 300;
 inputEl.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
 
 function searchCountries(e) {
-  if (e.target.value.trim().length < 2) {
+  if (!e.target.value.trim()) {
     onInnerHtmlCliner(countriesEl, countriesLestEl);
-    Notify.info('Too many matches found. Please enter a more specific name.');
     return;
   }
 
   const name = e.target.value;
 
   fetchCountries(name)
-    .then(countries => {
-      onInnerHtmlCliner(countriesEl, countriesLestEl);
-      if (countries.length === 1) {
-        countriesEl.innerHTML = countrieHbs(countries[0]);
-        return;
-      } 
-        countries.slice(0, 10).forEach(country => 
-          countriesLestEl.insertAdjacentHTML("beforeend",countriesHbs(country))
-        );
-    })
+    .then(countriesInHtml)
     .catch(error => {
       onInnerHtmlCliner(countriesEl, countriesLestEl);
       Notify.failure('Oops, there is no country with that name');
@@ -44,4 +34,20 @@ function onInnerHtmlCliner(...objects) {
   for (const object of objects) {
     object.innerHTML = '';
   }
+}
+
+function countriesInHtml(countries) {
+      onInnerHtmlCliner(countriesEl, countriesLestEl);
+      if (countries.length === 1) {
+        countriesEl.innerHTML = countrieHbs(countries[0]);
+        return;
+      }
+      if (countries.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      }
+      countries
+        .forEach(country =>
+          countriesLestEl.insertAdjacentHTML('beforeend', countriesHbs(country))
+        )
 }
