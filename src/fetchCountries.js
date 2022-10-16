c4a06ae48320db169d79924645071e0af0a8cc3f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-16 19:12:05 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 export default function fetchCountries(name) {
   return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
-    //   if (!response.ok) {
-    //     throw new Error(response.status);
-    //   }
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
     return response.json();
   });
 }
